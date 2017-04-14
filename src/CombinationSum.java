2ae92e28925ed74e24ccbb4abac6fa20ef0130c9
Jiamin Ding <jmding0714@gmail.com>
2017-04-14 00:32:39 -0500

    Updates and comments

--- src/CombinationSum.java
+++ src/CombinationSum.java
@@ -8,34 +8,14 @@ import java.util.List;
 public class CombinationSum {
 
 
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (candidates == null || candidates.length == 0) return result;
-        backtrack(result,new ArrayList<>(),candidates,target,0);
-        return result;
-    }
-
-    public static void backtrack(List<List<Integer>> result, List<Integer> temp, int[] candidates, int remain, int start){
-        if (remain < 0) return;
-        else if (remain == 0) result.add(new ArrayList<Integer>(temp));
-        else {
-            for (int i = start; i < candidates.length; i++){
-                temp.add(candidates[i]);
-                backtrack(result,temp,candidates,remain-candidates[i],i);
-                temp.remove(temp.size() - 1);
-            }
-        }
-    }
-
-
-    public static List<List<Integer>> combinationSum2(int[] nums, int target) {
+    public static List<List<Integer>> combinationSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        backtrack2(list, new ArrayList<>(), nums, target, 0);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
         return list;
     }
 
-    private static void backtrack2(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
         // If the distance to the target is less than 0, we exceeds the target. Return.
         if(remain < 0) return;
         // If the distance to the target is 0, means we reach the target. Add the temp list to the result list.
@@ -57,7 +37,7 @@ public class CombinationSum {
     public static void main(String[] args) {
         int[] candidates = new int[]{2,3,6,7};
         int target = 7;
-        List<List<Integer>> result = combinationSum2(candidates,target);
+        List<List<Integer>> result = combinationSum(candidates,target);
         System.out.println("done");
     }
 }
